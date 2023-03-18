848d5ac1ada0d21ea0ba7ff5b8a62c6d7de75734
Younes Nourzehi <unesnz@gmail.com>
2023-03-19 00:38:49 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Code your solutions in this file
+
+function writeCards(names, eventName){
+    const cardMessages = [];
+    for(let i=0 ; i<names.length ; i++){
+        cardMessages.push(`Thank you, ${names[i]}, for the wonderful ${eventName} gift!`)
+    }
+    return cardMessages;
+}
+
+function countDown(num){
+    if (num<0) console.error("The Number should be a positive integer!")
+    while(num>=0) {
+        console.log(num);
+        num--;
+    }
+}
